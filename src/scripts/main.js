4c75a3bfd4e1231116224867106191d94021e6eb
Shinnosuke Komiya <kurohune538@gmail.com>
2016-03-05 00:12:53 +0900

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,22 @@
 import { Router, Route, Link } from "react-router";
 import React from "react";
 
-const Header = React.createClass({
+const Top = React.createClass({
 	render() {
 		return (
 			<div>hoge</div>
 		);
 	}
 });
+
+render((
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<Route path="top" component={Top} />
+			<Route path="users" component={users}>
+				<Route path="/user/:userId" component={User} />
+			</Route>
+			<Route path="*" component={NoMatch} />
+		</Route>
+	</Router>
+),document.body);
